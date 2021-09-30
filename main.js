5fa617ae187fcf3ab8aa13f92a7a37b6520faccb
Morton Fox <github@qslw.com>
2021-09-29 23:02:45 -0400

    Update for CVS site changes

--- main.js
+++ main.js
@@ -1,32 +1,39 @@
+// jshint esversion: 6
+
 function runSelect(event) {
   event.preventDefault();
 
-  // Click on every "load to card" button.
-  var load2crd = document.getElementsByClassName('send_to_card_coupon');
-  for (var btn of load2crd) {
+  // Click on every "send to card" button.
+  let send2crd = document.getElementsByClassName('action-items');
+  console.log(send2crd.length + ' coupons found');
+
+  let clicked = 0;
+  for (let btn of send2crd) {
     btn.click();
+    clicked++;
   }
+  console.log(clicked + ' coupons clicked');
 }
 
 function insertButton(btn) {
   function waitForSite() {
-    var app_container = document.getElementById('coupon_filter');
-    if (app_container != null) {
+    let targetElem = document.getElementsByClassName('coupon-list-container');
+    if (targetElem != null && targetElem[0] !== undefined) {
       clearInterval(waitForSiteTimer);
-      app_container.insertBefore(btn, app_container.firstChild);
+      targetElem[0].insertBefore(btn, targetElem[0].firstChild);
     }
   }
 
-  // Wait for site to finish loading the app container before inserting button.
-  var waitForSiteTimer = setInterval(waitForSite, 100);
+  // Wait for site to finish loading the coupon list container before inserting button.
+  let waitForSiteTimer = setInterval(waitForSite, 100);
 }
 
 function init() {
   // Make a new button for our action.
-  var newbutton = document.createElement('button');
+  let newbutton = document.createElement('button');
   newbutton.name = 'send_all_to_card';
   newbutton.id = 'send_all_to_card';
-  newbutton.style.cssText = 'background-color: #E82A24; color: #fff; font-weight: 700; border: none; padding: 6px 10px; cursor: pointer;';
+  newbutton.style.cssText = 'background-color: #E82A24; color: #fff; font-weight: 700; margin: 5px; border: none; padding: 6px 10px; cursor: pointer;';
   newbutton.appendChild(document.createTextNode('Send All To Card'));
   newbutton.addEventListener('click', runSelect);
 
